aa485a4cd15ba6c3219b23f35661ab5d97e229f0
SebRVV <U202110299@upc.edu.pe>
2025-11-30 13:08:55 -0500

    feat: dependencias de supabase

---
+++ lib/supabase/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  const supabase = createMiddlewareClient({ req, res });
+
+  // Sin esto, el usuario no mantiene sesión
+  await supabase.auth.getSession();
+
+  return res;
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
